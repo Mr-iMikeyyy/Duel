79b427ebcc91e2eac547e892d178031b3e8a94ba
Mike Mccullough <mikesawesomevids@gmail.com>
2025-03-29 09:07:58 -0400

    Init
    Init

---
+++ src/main/java/net/madmike/duel/DuelClient.java
@@ -0,0 +1,10 @@
+package net.madmike.duel;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DuelClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
